263b592d475b9e2bc5c4a59eca06977796132ee2
ZeDudeZeDanY <daniil.ivanov.2000@mail.ru>
2020-08-30 20:47:10 +0500

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,6 @@ $app->get('/cpu', function (Request $request, Response $response, $args) use ($p
     $response = $response->withHeader("Content-Type", "application/json");
     return $response;
 });
-
 $app->get('/ssd', function (Request $request, Response $response, $args) use ($pdo){
     $stmt = $pdo->query('SELECT * FROM ssd');
     $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -39,6 +38,68 @@ $app->get('/hdd', function (Request $request, Response $response, $args) use ($p
     $response = $response->withHeader("Content-Type", "application/json");
     return $response;
 });
+
+$app->get('/monitor', function (Request $request, Response $response, $args) use ($pdo){
+    $stmt = $pdo->query('SELECT * FROM monitor');
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $response->getBody()->write(json_encode($data));
+    $response = $response->withHeader("Content-Type", "application/json");
+    return $response;
+});
+
+$app->get('/mouse', function (Request $request, Response $response, $args) use ($pdo){
+    $stmt = $pdo->query('SELECT * FROM mouse');
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $response->getBody()->write(json_encode($data));
+    $response = $response->withHeader("Content-Type", "application/json");
+    return $response;
+});
+
+$app->get('/motherboard', function (Request $request, Response $response, $args) use ($pdo){
+    $stmt = $pdo->query('SELECT * FROM motherboard');
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $response->getBody()->write(json_encode($data));
+    $response = $response->withHeader("Content-Type", "application/json");
+    return $response;
+});
+
+$app->get('/ram', function (Request $request, Response $response, $args) use ($pdo){
+    $stmt = $pdo->query('SELECT * FROM ram');
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $response->getBody()->write(json_encode($data));
+    $response = $response->withHeader("Content-Type", "application/json");
+    return $response;
+});
+
+$app->get('/psu', function (Request $request, Response $response, $args) use ($pdo){
+    $stmt = $pdo->query('SELECT * FROM psu');
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $response->getBody()->write(json_encode($data));
+    $response = $response->withHeader("Content-Type", "application/json");
+    return $response;
+});
+
+$app->get('/gpu', function (Request $request, Response $response, $args) use ($pdo){
+    $stmt = $pdo->query('SELECT * FROM gpu');
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $response->getBody()->write(json_encode($data));
+    $response = $response->withHeader("Content-Type", "application/json");
+    return $response;
+});
+
+$app->get('/vendor_list', function (Request $request, Response $response, $args) use ($pdo){
+    $stmt = $pdo->query('SELECT * FROM vendor_list');
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $response->getBody()->write(json_encode($data));
+    $response = $response->withHeader("Content-Type", "application/json");
+    return $response;
+});
+
+//$app->get('/customer/{id}', function (Request $request, Response $response) use ($pdo) {
+//    $route = $request->getAttribute('route');
+//    $id = $route->getArgument('id');
+//    return $id;
+//});
 
 //$app->get('/insert', function (Request $request, Response $response, $args) use ($pdo){
 //    $pdo->query("INSERT INTO customer(fname, surname, phone, email) VALUES('Andrey', 'Otinov', '83221488228', 'otinovoleg@hubbabubba.com')");
